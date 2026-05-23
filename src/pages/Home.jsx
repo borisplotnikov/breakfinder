e90d9d345e0c26db0b4b94f8ecdf226e747370d3
Boris Plotnikov <boriswo@gmail.com>
2026-05-23 11:22:30 -0700

    Arrange uploaders side-by-side

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,31 @@ import TextOutput from "../components/TextOutput";
 
 export default function Home() {
   return (
-    <div className="page">
-      <DataUploader />
-      <SchemaUploader />
-      <TextOutput />
+    <div className="container-fluid vh-100 d-flex flex-column p-4 overflow-hidden bg-light">
+      
+      {/* Top Section: Side-by-Side Uploaders */}
+      <div className="row flex-grow-1 g-4 mb-4 align-items-stretch">
+        <div className="col-md-6 d-flex">
+          <div className="card w-100 shadow-sm p-3">
+            <DataUploader />
+          </div>
+        </div>
+        <div className="col-md-6 d-flex">
+          <div className="card w-100 shadow-sm p-3">
+            <SchemaUploader />
+          </div>
+        </div>
+      </div>
+
+      {/* Bottom Section: Aligned Output Box */}
+      <div className="row">
+        <div className="col-12">
+          <div className="card shadow-sm p-3 text-dark bg-white" style={{ minHeight: "30vh" }}>
+            <TextOutput />
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
-
