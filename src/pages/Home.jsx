1683320deecb55a36d7b31c66b1a2584d726c675
Boris Plotnikov <boriswo@gmail.com>
2026-05-23 10:32:24 -0700

    Add Data and Schema uploaders

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
-import Uploader from "../components/Uploader/Uploader";
+import DataUploader from "../components/DataUploader";
+import SchemaUploader from "../components/SchemaUploader";
 
 export default function Home() {
   return (
     <div className="page">
       <h1>BreakFinder</h1>
-      <Uploader />
+      <DataUploader />
+      <SchemaUploader />
     </div>
   );
 }
