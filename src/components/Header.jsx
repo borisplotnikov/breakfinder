e7193401d37c3ba73f238ac56e3bf1d1e963e813
Boris Plotnikov <boriswo@gmail.com>
2026-06-02 17:34:51 -0700

    Add Bootstrap NavBar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,18 @@
 export default function Header() {
-  return <p>Header</p>;
+  return (
+    <nav className="navbar bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img
+            src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg" // Live URL
+            alt="Logo"
+            width="30"
+            height="24"
+            className="d-inline-block align-text-top me-2" // Added me-2 for a touch of spacing
+          />
+          Bootstrap
+        </a>
+      </div>
+    </nav>
+  );
 }
