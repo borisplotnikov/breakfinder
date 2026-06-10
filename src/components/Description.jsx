afc75f8aa1019fd205ee31ef932a4833df88d5b0
Boris Plotnikov <boriswo@gmail.com>
2026-06-10 07:51:58 -0700

    Adjust Description spacing

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -5,27 +5,22 @@ export default function Description() {
     <section className="col-12 col-md-4">
       <div className="card">
         <div className="card-body">
-          <h5 className="card-title text-center pb-3">
+          <h5 className="card-title text-center mb-4">
             Reverse-Schema for Parsing
           </h5>
-          <h6 className="card-subtitle mb-2 text-body-secondary">
+          <h6 className="card-subtitle text-body-secondary">
             What we've bult:
           </h6>
-          <p className="card-text mb-1">
+          <p className="card-text px-3 mb-4" style={{ textAlign: "justify" }}>
             This tool allows you to easily analyze and find structural patterns
             within your datasets. Simply upload your data file (JSON, CSV, or
-            XML) along with a target schema file to get started.
+            XML) along with a target schema file to get started. Once loaded,
+            the application will scan your data and return a precise list of
+            locations and line numbers where the specified schema structure is
+            found.
           </p>
-          <p className="card-text">
-            Once loaded, the application will scan your data and return a
-            precise list of locations and line numbers where the specified
-            schema structure is found.
-          </p>
-
-          <h6 className="card-subtitle mb-2 text-body-secondary">
-            How did we do:
-          </h6>
-          <p className="card-text">6th place out of 12</p>
+          <h6 className="card-subtitle text-body-secondary">How did we do:</h6>
+          <p className="card-text px-3">6th place out of 12</p>
           <div className="card-body">
             <CardGrid />
           </div>
